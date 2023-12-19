f3a6d77b1e0360c26c27ccc2df3bf5633e6686ed
superscman <vdcf2000@gmail.com>
2023-12-19 18:33:11 +0900

    30분씩 찔끔찔끔 공부..

--- src/main/java/superscman/hellospring/Repository/JdbcMemberRepository.java
+++ src/main/java/superscman/hellospring/Repository/JdbcMemberRepository.java
@@ -1,4 +1,126 @@
 package superscman.hellospring.Repository;
 
-public class JdbcMemberRepository {
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import superscman.hellospring.domain.Member;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class JdbcMemberRepository implements MemberRepository {
+
+    private final DataSource dataSource;
+
+    public JdbcMemberRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public Member save(Member member) {
+
+        String sql = "insert into member(name) values(?)";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = dataSource.getConnection();
+            pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            pstmt.setString(1, member.getName());
+
+            pstmt.executeUpdate(); // 실제 쿼리가 이 때 날아감
+            rs = pstmt.getGeneratedKeys();
+
+            if(rs.next()) {
+                member.setId(rs.getLong(1));
+            }else {
+                throw new SQLException("id 조회 실패");
+            }
+
+            return member;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }finally {
+            close(conn, pstmt, rs);
+        }
+
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        String sql = "select * from member where id = ?";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = dataSource.getConnection();
+            pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            pstmt.setLong(1, id);
+
+            pstmt.executeQuery(); // 실제 쿼리가 이 때 날아감
+
+            if(rs.next()) {
+                Member member = new Member();
+                member.setId(rs.getLong("id"));
+                member.setName(rs.getString("name"));
+                return Optional.of(member);
+            }else {
+                throw new SQLException("id 조회 실패");
+            }
+
+            return member;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }finally {
+            close(conn, pstmt, rs);
+        }
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+
+        return null;
+    }
+
+    private Connection getConnection() {
+        return DataSourceUtils.getConnection(dataSource);
+    }
+
+    private void close(Connection conn, PreparedStatement pstmt, ResultSet rs) {
+        try{
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try{
+            if (pstmt != null) {
+                pstmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try{
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
