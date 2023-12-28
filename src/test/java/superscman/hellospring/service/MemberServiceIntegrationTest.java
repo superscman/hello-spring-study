1be66f4eeffdeeeed2385b3f40da6c28f0537f02
superscman <vdcf2000@gmail.com>
2023-12-28 18:34:59 +0900

    db 연결 에러 오졌다리..내일고쳐야지

---
+++ src/test/java/superscman/hellospring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,52 @@
+package superscman.hellospring.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import superscman.hellospring.Repository.MemberRepository;
+import superscman.hellospring.Repository.MemoryMemberRepository;
+import superscman.hellospring.domain.Member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+        //when
+        long saveId = memberService.join(member);
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+        assertThrows(RuntimeException.class, () -> memberService.join(member2));
+
+    }
+
+}
